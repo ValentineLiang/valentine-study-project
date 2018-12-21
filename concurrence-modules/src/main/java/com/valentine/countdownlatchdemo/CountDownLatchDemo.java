ba476698589dbdca2a7121a5519c5230c4b1ace1
Valentine <416818236@qq.com>
2018-12-21 14:43:55 +0800

    commit

--- concurrence-modules/src/main/java/com/valentine/countdownlatchdemo/CountDownLatchDemo.java
+++ concurrence-modules/src/main/java/com/valentine/countdownlatchdemo/CountDownLatchDemo.java
@@ -8,13 +8,11 @@ import java.util.concurrent.CountDownLatch;
  */
 public class CountDownLatchDemo {
 
-
-
     public static void main(String[] args) throws InterruptedException {
         // 定义一个倒计时，每个减一,0的时候才会让主线程释放
         CountDownLatch countDownLatch = new CountDownLatch(3);
-        new Thread(()->{
 
+        new Thread(()->{
             countDownLatch.countDown();
         },"1").start();
 
@@ -25,6 +23,7 @@ public class CountDownLatchDemo {
         new Thread(()->{
             countDownLatch.countDown();
         },"3").start();
+
         // 阻塞主线程
         countDownLatch.await();
         System.out.println("执行完毕");
